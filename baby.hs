afc43e9ae3e6435ba4891a27cdca305f987a401e
Hans Ott <hansott@hotmail.be>
2016-01-03 12:48:36 +0100

    Add notes.

--- baby.hs
+++ baby.hs
@@ -1,7 +1,23 @@
 doubleMe x = x + x
 
-doubleUs x y = doubleMe x + doubleMe y
+-- bad:
+doubleUs x y = x * 2 + y * 2
+-- better:
+doubleUs' x y = doubleMe x + doubleMe y
 
+-- else is mandatory
 doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
+
+-- ' is a valid character, we usually use ' to denote either
+-- a strict version of a function, or a slightly modified version
+-- of a function or variable with a smiliar name
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+
+-- conanO'Brien is a valid function name
+-- Note: functions can't begin with capital letters
+-- Note: the function doesn't take any parameters
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+
