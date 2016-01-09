0d23136b00cf15dec69e0081bf1595d0cd806668
Hans Ott <hansott@hotmail.be>
2016-01-09 19:57:14 +0100

    Add more examples of pattern matching

--- chapter3-syntax-in-functions/pattern-matching.hs
+++ chapter3-syntax-in-functions/pattern-matching.hs
@@ -8,3 +8,18 @@ sayMe 2 = "Two!"
 sayMe 3 = "Three!"
 -- "sayMe 3" outputs "Three!"
 -- "sayMe 4" outputs "*** Exception: chapter3-syntax-in-functions/pattern-matching.hs:(6,1)-(8,18): Non-exhaustive patterns in function sayMe"
+
+-- can also be implemented as "factorial n = product [1..n]"
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors
+    :: (Double, Double) -> (Double, Double) -> (Double, Double)
+-- BAD:
+-- addVectors a b = (fst a + fst b, snd a + snd b)
+-- GOOD (Increases readability!):
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+third :: (a, b, c) -> c
+third (_, _, c) = c
